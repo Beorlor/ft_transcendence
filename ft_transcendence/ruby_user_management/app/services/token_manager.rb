a8e4feaafe1bca5dfeaea1140606e0310ac939cc
Beorlor <jbdurand2000@gmail.com>
2024-09-07 05:13:01 +0200

    gitignore

---
+++ ft_transcendence/ruby_user_management/app/services/token_manager.rb
@@ -0,0 +1,43 @@
+require 'jwt'
+require 'json'
+
+module TokenManager
+  SECRET_KEY = ENV['SECRET_KEY']
+
+  def self.generate_tokens(user_data)
+    id_token = encode({ user_id: 1 }, exp: 3600)
+    access_token = encode({ user_id: 1 }, exp: 600)
+    refresh_token = encode({ user_id: 1 }, exp: 604800)
+
+    { id_token: id_token, access_token: access_token, refresh_token: refresh_token }
+  end
+
+  def self.verify_access_token(token)
+    decode(token)
+  end
+
+  def self.refresh_access_token(refresh_token)
+    decoded_token = decode(refresh_token)
+    if decoded_token
+      encode({ user_id: decoded_token['user_id'] }, exp: 600)  # New access token for 10 minutes
+    else
+      nil
+    end
+  end
+
+  private
+
+  def self.encode(payload, exp: 3600)
+    payload[:exp] = Time.now.to_i + exp
+    JWT.encode(payload, SECRET_KEY, 'HS256')
+  end
+
+  def self.decode(token)
+    begin
+      decoded = JWT.decode(token, SECRET_KEY, true, { algorithm: 'HS256' })
+      decoded[0]
+    rescue
+      nil
+    end
+  end
+end
