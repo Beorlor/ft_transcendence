2f56a83aa412e84267ae78581e0faf2dc431a121
ZomDev3343 <contact.zomzd@gmail.com>
2024-09-09 17:40:50 +0200

    Fix logging

--- ft_transcendence/ruby_ssr/app.rb
+++ ft_transcendence/ruby_ssr/app.rb
@@ -1,5 +1,6 @@
 require 'webrick'
 require 'erb'
+require_relative 'app/log/custom_logger'
 
 server = WEBrick::HTTPServer.new(:Port => 4568)
 
@@ -17,6 +18,7 @@ server.mount_proc '/game' do |req, res|
 	template = ERB.new(File.read("view/index.erb"))
 	res.body = template.result(binding)
 	res.content_type = "text/html"
+	CustomLogger.log("ofhewohfoh")
 end
 
 trap 'INT' do server.shutdown end
