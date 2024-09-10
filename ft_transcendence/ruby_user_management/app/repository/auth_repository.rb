5537b357b7fb15d4123cfcd8ed1408d43f153501
Thomas <thomasribalta@gmail.com>
2024-09-11 00:04:54 +0200

    edit logger module, add check for userwith42

--- ft_transcendence/ruby_user_management/app/repository/auth_repository.rb
+++ ft_transcendence/ruby_user_management/app/repository/auth_repository.rb
@@ -1,9 +1,17 @@
 require_relative '../config/database'
+require_relative '../log/custom_logger'
 
 module AuthRepository
 
   def self.registerUser42(user_info)
+    Logger.log('AuthRepository', "Registering user with email #{user_info[:email]}")
+    user = Database.get_one_element_from_table('_user', 'email', user_info[:email])
+    if user.length > 0
+      Logger.log('AuthRepository', "User with email #{user_info[:email]} already exists")
+      return
+    end
     Database.insert_into_table('_user', user_info)
+    Logger.log('AuthRepository', "User with email #{user_info[:email]} registered")
   end
 
 end
