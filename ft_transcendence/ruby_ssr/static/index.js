8f94780de6a00366064230a799a5b140d33a988b
ZomDev3343 <contact.zomzd@gmail.com>
2024-09-11 16:23:46 +0200

    On avance

--- ft_transcendence/ruby_ssr/static/index.js
+++ ft_transcendence/ruby_ssr/static/index.js
@@ -1,10 +1,65 @@
-document.addEventListener("DOMContentLoaded", () => {
+const WINDOW_EVENTS = {};
+const WINDOW_ANIMATIONS_FRAMES = [];
+
+const addListener = (event, handler) => {
+	if (!(event in WINDOW_EVENTS))
+		WINDOW_EVENTS[event] = [];
+	WINDOW_EVENTS[event] = handler;
+	window.addEventListener(event, handler);
+};
+
+const removeAllListeners = (event) => {
+	if (!(event in WINDOW_EVENTS))
+		return ;
+	for (handler in WINDOW_EVENTS[event])
+		window.removeEventListener(event, handler);
+	delete WINDOW_EVENTS[event];
+};
+
+const cancelAnimations = () => {
+	WINDOW_ANIMATIONS_FRAMES.forEach((v, i, a) => {
+		window.cancelAnimationFrame(v);
+	});
+	WINDOW_ANIMATIONS_FRAMES.length = 0;
+};
+
+document.addEventListener("DOMContentLoaded", (ev) => {
 	document.getElementById("pong_link").addEventListener("click",
 		function (){
 			fetch("/pong")
 				.then(res => res.text())
 				.then(html =>{
-					document.getElementById("game").innerHTML = html;
+					const game = document.getElementById("game");
+					cancelAnimations();
+					removeAllListeners("keydown");
+					removeAllListeners("keyup");
+					game.innerHTML = html;
+
+					const script = game.querySelector('script');
+					const newScript = document.createElement('script');
+					newScript.type = 'module';
+					newScript.src = script.src;
+					game.appendChild(newScript);
+				})
+				.catch(error => console.error("Oh l'erreur !", error));
+		}
+	);
+	document.getElementById("aipong_link").addEventListener("click",
+		function (){
+			fetch("/aipong")
+				.then(res => res.text())
+				.then(html =>{
+					const game = document.getElementById("game");
+					cancelAnimations();
+					removeAllListeners("keydown");
+					removeAllListeners("keyup");
+					game.innerHTML = html;
+
+					const script = game.querySelector('script');
+					const newScript = document.createElement('script');
+					newScript.type = 'module';
+					newScript.src = script.src;
+					game.appendChild(newScript);
 				})
 				.catch(error => console.error("Oh l'erreur !", error));
 		}
