3f1f933f0000bc412dee9d68f615041ee5c44891
zom <contact.zomzd@gmail.com>
2024-09-18 14:45:21 +0200

    Fixed pong deltatime

--- ft_transcendence/ruby_ssr/static/pong/constants.mjs
+++ ft_transcendence/ruby_ssr/static/pong/constants.mjs
@@ -6,15 +6,15 @@ const	winningScore = 2;
 let		barWidth = 12;
 let		barHeight = 120;
 const	barPadding = 10;
-const	barMoveSpeed = 200;
+const	barMoveSpeed = 750;
 const	barHitboxPadding = 3;
 
 const	topHitbox = 10;
 
 let		ballRadius = 8;
-let		ballMoveSpeed = 12;
-let		ballMaxSpeed = 64;
-let		ballAcceleration = 0.30;
+let		ballMoveSpeed = 25;
+let		ballMaxSpeed = 75;
+let		ballAcceleration = 3;
 
 const	goalWidth = 50;
 
