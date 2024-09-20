db6bee5f7db9ec20d42e73880e3d2bf408a77966
ZomDev3343 <contact.zomzd@gmail.com>
2024-09-20 15:25:49 +0200

    Fix cors problem Fix nginx bypass WIP ssr page loading

--- ft_transcendence/ruby_ssr/static/validate_code.js
+++ ft_transcendence/ruby_ssr/static/validate_code.js
@@ -1,35 +1,35 @@
 document
-  .getElementById("form_validate_code")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+	.getElementById("form_validate_code")
+	.addEventListener("submit", function (event) {
+		event.preventDefault();
 
-    const popUp = document.getElementById("pop-up");
-    popUp.innerHTML = "";
+		const popUp = document.getElementById("pop-up");
+		popUp.innerHTML = "";
 
-    const formData = new FormData(this);
-    const formObject = {};
-    formData.forEach((value, key) => {
-      formObject[key] = value;
-    });
+		const formData = new FormData(this);
+		const formObject = {};
+		formData.forEach((value, key) => {
+			formObject[key] = value;
+		});
 
-    fetch("http://localhost:4567/auth/validate-code", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        access_token: localStorage.getItem("access_token"),
-      },
-      body: JSON.stringify(formObject),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        if (data.success) {
-          console.log("Lezzz goo!");
-        } else {
-          popUp.innerHTML = `<div class="alert alert-danger" role="alert">
+		fetch("https://localhost/auth/validate-code", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				access_token: localStorage.getItem("access_token"),
+			},
+			body: JSON.stringify(formObject),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				if (data.success) {
+					console.log("Lezzz goo!");
+				} else {
+					popUp.innerHTML = `<div class="alert alert-danger" role="alert">
               ${data.error}
               </div>`;
-        }
-      })
-      .catch((error) => console.error("Error:", error));
-  });
+				}
+			})
+			.catch((error) => console.error("Error:", error));
+	});
