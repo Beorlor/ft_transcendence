041825ba80cd6e6d79147f30b90387a72b8e99b0
ZomDev3343 <contact.zomzd@gmail.com>
2024-09-20 16:56:15 +0200

    SSR seems to be okay

--- ft_transcendence/ruby_ssr/static/index.js
+++ ft_transcendence/ruby_ssr/static/index.js
@@ -30,97 +30,99 @@ window.cancelAnimations = () => {
 function loadScript() {
 	if (window.GAMESTATE > -1)
 		return;
-	document.getElementById("home_link").addEventListener("click", function () {
-		const popUp = document.getElementById("pop-up");
-		popUp.innerHTML = "";
-		window.GAMESTATE = window.GAME_STATES.default;
-		window.cancelAnimations();
-		const game = document.getElementById("game");
-		game.innerHTML = "";
-	});
-
-	document.getElementById("pong_link").addEventListener("click", function () {
-		const popUp = document.getElementById("pop-up");
-		popUp.innerHTML = "";
-		fetch("https://localhost/pong")
-			.then((res) => res.text())
-			.then((html) => {
-				const game = document.getElementById("game");
-				window.GAMESTATE = window.GAME_STATES.pong;
-				game.innerHTML = html;
-				document.getElementById("game_name").textContent = "Pongpong";
-
-				const script = game.querySelector("script");
-				const newScript = document.createElement("script");
-				newScript.type = "module";
-				newScript.src = script.src;
-				game.appendChild(newScript);
-			})
-			.catch((error) => console.error("Oh l'erreur !", error));
-	});
-
-	document.getElementById("aipong_link").addEventListener("click", function () {
-		const popUp = document.getElementById("pop-up");
-		popUp.innerHTML = "";
-		fetch("https://localhost/pong")
-			.then((res) => res.text())
-			.then((html) => {
-				const game = document.getElementById("game");
-				window.GAMESTATE = window.GAME_STATES.aipong;
-				game.innerHTML = html;
-				document.getElementById("game_name").textContent = "AI Pongpong";
-
-				const script = game.querySelector("script");
-				const newScript = document.createElement("script");
-				newScript.type = "module";
-				newScript.src = script.src;
-				game.appendChild(newScript);
-			})
-			.catch((error) => console.error("Oh l'erreur !", error));
-	});
-
-	document
-		.getElementById("button_login")
-		.addEventListener("click", function () {
+	if (document.getElementById("game")) {
+		document.getElementById("home_link").addEventListener("click", function () {
 			const popUp = document.getElementById("pop-up");
 			popUp.innerHTML = "";
-			fetch("https://localhost/ssr/login")
+			window.GAMESTATE = window.GAME_STATES.default;
+			window.cancelAnimations();
+			const game = document.getElementById("game");
+			game.innerHTML = "";
+		});
+
+		document.getElementById("pong_link").addEventListener("click", function () {
+			const popUp = document.getElementById("pop-up");
+			popUp.innerHTML = "";
+			fetch("https://localhost/pong")
 				.then((res) => res.text())
 				.then((html) => {
-					window.GAMESTATE = window.GAME_STATES.default;
-					window.cancelAnimations();
 					const game = document.getElementById("game");
+					window.GAMESTATE = window.GAME_STATES.pong;
 					game.innerHTML = html;
+					document.getElementById("game_name").textContent = "Pongpong";
 
 					const script = game.querySelector("script");
 					const newScript = document.createElement("script");
 					newScript.type = "module";
 					newScript.src = script.src;
 					game.appendChild(newScript);
-				});
+				})
+				.catch((error) => console.error("Oh l'erreur !", error));
 		});
 
-	document
-		.getElementById("button_register")
-		.addEventListener("click", function () {
+		document.getElementById("aipong_link").addEventListener("click", function () {
 			const popUp = document.getElementById("pop-up");
 			popUp.innerHTML = "";
-			fetch("https://localhost/ssr/register")
+			fetch("https://localhost/pong")
 				.then((res) => res.text())
 				.then((html) => {
-					window.GAMESTATE = window.GAME_STATES.default;
-					window.cancelAnimations();
 					const game = document.getElementById("game");
+					window.GAMESTATE = window.GAME_STATES.aipong;
 					game.innerHTML = html;
+					document.getElementById("game_name").textContent = "AI Pongpong";
 
 					const script = game.querySelector("script");
 					const newScript = document.createElement("script");
 					newScript.type = "module";
 					newScript.src = script.src;
 					game.appendChild(newScript);
-				});
+				})
+				.catch((error) => console.error("Oh l'erreur !", error));
 		});
-	window.GAMESTATE = 0;
+
+		document
+			.getElementById("button_login")
+			.addEventListener("click", function () {
+				const popUp = document.getElementById("pop-up");
+				popUp.innerHTML = "";
+				fetch("https://localhost/ssr/login")
+					.then((res) => res.text())
+					.then((html) => {
+						window.GAMESTATE = window.GAME_STATES.default;
+						window.cancelAnimations();
+						const game = document.getElementById("game");
+						game.innerHTML = html;
+
+						const script = game.querySelector("script");
+						const newScript = document.createElement("script");
+						newScript.type = "module";
+						newScript.src = script.src;
+						game.appendChild(newScript);
+					});
+			});
+
+		document
+			.getElementById("button_register")
+			.addEventListener("click", function () {
+				const popUp = document.getElementById("pop-up");
+				popUp.innerHTML = "";
+				fetch("https://localhost/ssr/register")
+					.then((res) => res.text())
+					.then((html) => {
+						window.GAMESTATE = window.GAME_STATES.default;
+						window.cancelAnimations();
+						const game = document.getElementById("game");
+						game.innerHTML = html;
+
+						const script = game.querySelector("script");
+						const newScript = document.createElement("script");
+						newScript.type = "module";
+						newScript.src = script.src;
+						game.appendChild(newScript);
+					});
+			});
+			window.GAMESTATE = 0;
+	}
 }
 
 document.addEventListener("DOMContentLoaded", (ev) => {
