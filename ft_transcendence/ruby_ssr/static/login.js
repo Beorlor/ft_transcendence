7e466a6e94f8ec9ce7c4adaeeef27329f01be95d
Thomas <thomasribalta@gmail.com>
2024-09-22 13:22:51 +0200

    add loading

--- ft_transcendence/ruby_ssr/static/login.js
+++ ft_transcendence/ruby_ssr/static/login.js
@@ -1,49 +1,51 @@
 function loadLoginFormAction() {
-	if (document.getElementById("form_login")) {
-		document
-			.getElementById("form_login")
-			.addEventListener("submit", function (event) {
-				event.preventDefault();
+  if (document.getElementById("form_login")) {
+    document
+      .getElementById("form_login")
+      .addEventListener("submit", function (event) {
+        event.preventDefault();
 
-				const popUp = document.getElementById("pop-up");
-				popUp.innerHTML = "";
+        const popUp = document.getElementById("pop-up");
+        popUp.innerHTML = "";
 
-				const formData = new FormData(this);
-				const formObject = {};
-				formData.forEach((value, key) => {
-					formObject[key] = value;
-				});
+        const formData = new FormData(this);
+        const formObject = {};
+        formData.forEach((value, key) => {
+          formObject[key] = value;
+        });
 
-				fetch("https://localhost/auth/login", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json"
-					},
-					body: JSON.stringify(formObject),
-				})
-					.then((response) => response.json())
-					.then((data) => {
-						console.log(data);
-						if (data.success) {
-							localStorage.setItem("access_token", data.access_token);
-							window.loadPage(document.getElementById("game"),
-								"https://localhost/validate-code",
-								window.GAME_STATES.default);
-						} else {
-							popUp.innerHTML = `<div class="alert alert-danger" role="alert">
+        fetch("https://localhost/auth/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formObject),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            if (data.success) {
+              localStorage.setItem("Authorization", data.access_token);
+              window.loadPage(
+                document.getElementById("game"),
+                "https://localhost/validate-code",
+                window.GAME_STATES.default
+              );
+            } else {
+              popUp.innerHTML = `<div class="alert alert-danger" role="alert">
               ${data.error}
               </div>`;
-						}
-					})
-					.catch((error) => console.error("Error:", error));
-			});
-	}
+            }
+          })
+          .catch((error) => console.error("Error:", error));
+      });
+  }
 }
 
 document.addEventListener("DOMContentLoaded", (_) => {
-	if (!document.getElementById("game")) {
-		loadLoginFormAction();
-	}
+  if (!document.getElementById("game")) {
+    loadLoginFormAction();
+  }
 });
 
 loadLoginFormAction();
