285ba192cc8381fb5fc6455f62c8b89e892bee8c
Thomas <thomasribalta@gmail.com>
2024-09-22 13:22:51 +0200

    add loading

--- ft_transcendence/ruby_ssr/static/index.js
+++ ft_transcendence/ruby_ssr/static/index.js
@@ -2,120 +2,124 @@ const WINDOW_EVENTS = {};
 
 window.WINDOW_ANIMATIONS_FRAMES = [];
 window.GAME_STATES = {
-	default: 0,
-	pong: 1,
-	aipong: 2,
-	threejs: 3,
+  default: 0,
+  pong: 1,
+  aipong: 2,
+  threejs: 3,
 };
 
 window.GAMESTATE = -1;
 
 window.addListener = (event, handler) => {
-	if (!(event in WINDOW_EVENTS)) WINDOW_EVENTS[event] = [];
-	WINDOW_EVENTS[event] = handler;
-	window.addEventListener(event, handler);
+  if (!(event in WINDOW_EVENTS)) WINDOW_EVENTS[event] = [];
+  WINDOW_EVENTS[event] = handler;
+  window.addEventListener(event, handler);
 };
 
 window.removeAllListeners = (event) => {
-	if (!(event in WINDOW_EVENTS)) return;
-	for (let handler of WINDOW_EVENTS[event])
-		window.removeEventListener(event, handler);
-	history.pushState()
-	delete WINDOW_EVENTS[event];
+  if (!(event in WINDOW_EVENTS)) return;
+  for (let handler of WINDOW_EVENTS[event])
+    window.removeEventListener(event, handler);
+  history.pushState();
+  delete WINDOW_EVENTS[event];
 };
 
 window.cancelAnimations = () => {
-	for (let v in WINDOW_ANIMATIONS_FRAMES) window.cancelAnimationFrame(v);
-	WINDOW_ANIMATIONS_FRAMES.length = 0;
+  for (let v in WINDOW_ANIMATIONS_FRAMES) window.cancelAnimationFrame(v);
+  WINDOW_ANIMATIONS_FRAMES.length = 0;
 };
 
 window.resetHomePage = function () {
-	const popUp = document.getElementById("pop-up");
-	popUp.innerHTML = "";
-	window.GAMESTATE = window.GAME_STATES.default;
-	window.cancelAnimations();
-	const game = document.getElementById("game");
-	game.innerHTML = "";
+  const popUp = document.getElementById("pop-up");
+  popUp.innerHTML = "";
+  window.GAMESTATE = window.GAME_STATES.default;
+  window.cancelAnimations();
+  const game = document.getElementById("game");
+  game.innerHTML = "";
 };
 
-window.loadPageScript = function (game){
-	const script = game.querySelector("script");
-	if (!script)
-		return ;
-	const newScript = document.createElement("script");
-	newScript.type = "module";
-	newScript.src = script.src;
-	game.appendChild(newScript);
+window.loadPageScript = function (game) {
+  const script = game.querySelector("script");
+  if (!script) return;
+  const newScript = document.createElement("script");
+  newScript.type = "module";
+  newScript.src = script.src;
+  game.appendChild(newScript);
 };
 
-function loadPage(game, url, gamestate){
-	window.GAMESTATE = gamestate;
-	history.pushState(null, null, url);
-	fetch(url, {headers: {'X-Requested-With': 'XMLHttpRequest', 'access_token': localStorage.getItem('access_token')}})
-		.then(res => res.text())
-		.then(html => {
-			game.innerHTML = html;
-			window.loadPageScript(game);
-		})
-		.catch(err => console.error("Error: ", err));
+function loadPage(game, url, gamestate) {
+  window.GAMESTATE = gamestate;
+  history.pushState(null, null, url);
+  fetch(url, {
+    headers: {
+      "X-Requested-With": "XMLHttpRequest",
+      Authorization: localStorage.getItem("Authorization"),
+    },
+  })
+    .then((res) => res.text())
+    .then((html) => {
+      game.innerHTML = html;
+      window.loadPageScript(game);
+    })
+    .catch((err) => console.error("Error: ", err));
 }
 
 function loadScript() {
-	if (window.GAMESTATE > -1)
-		return;
-	let game = document.getElementById("game");
-	if (game) {
+  if (window.GAMESTATE > -1) return;
+  let game = document.getElementById("game");
+  if (game) {
+    document.getElementById("home_link").addEventListener("click", (ev) => {
+      ev.preventDefault();
+      const url = "https://localhost";
+      loadPage(game, url, window.GAME_STATES.default);
+    });
 
-		document.getElementById("home_link").addEventListener("click", (ev) => {
-			ev.preventDefault();
-			const url = "https://localhost";
-			loadPage(game, url, window.GAME_STATES.default);
-		});
+    document.getElementById("pong_link").addEventListener("click", (ev) => {
+      ev.preventDefault();
 
-		document.getElementById("pong_link").addEventListener("click", (ev) => {
-			ev.preventDefault();
+      const url = "https://localhost/pong";
+      loadPage(game, url, window.GAME_STATES.pong);
+    });
 
-			const url = "https://localhost/pong";
-			loadPage(game, url, window.GAME_STATES.pong);
-		});
+    document.getElementById("aipong_link").addEventListener("click", (ev) => {
+      ev.preventDefault();
 
-		document.getElementById("aipong_link").addEventListener("click", (ev) => {
-			ev.preventDefault();
+      const url = "https://localhost/pong";
+      loadPage(game, url, window.GAME_STATES.aipong);
+    });
 
-			const url = "https://localhost/pong";
-			loadPage(game, url, window.GAME_STATES.aipong);
-		});
+    document.getElementById("button_login").addEventListener("click", (ev) => {
+      ev.preventDefault();
 
-		document.getElementById("button_login").addEventListener("click", (ev) => {
-			ev.preventDefault();
+      const url = "https://localhost/ssr/login";
+      loadPage(game, url, window.GAME_STATES.default);
+    });
 
-			const url = "https://localhost/ssr/login";
-			loadPage(game, url, window.GAME_STATES.default);
-		});
+    document
+      .getElementById("button_register")
+      .addEventListener("click", (ev) => {
+        ev.preventDefault();
 
-		document.getElementById("button_register").addEventListener("click", (ev) => {
-			ev.preventDefault();
+        const url = "https://localhost/ssr/register";
+        loadPage(game, url, window.GAME_STATES.default);
+      });
 
-			const url = "https://localhost/ssr/register";
-			loadPage(game, url, window.GAME_STATES.default);
-		});
+    window.GAMESTATE = 0;
+  }
+}
 
-		window.GAMESTATE = 0;
-	}
-};
+window.addEventListener("popstate", function (_) {
+  const currentUrl = window.location.pathname;
 
-window.addEventListener('popstate', function(_) {
-	const currentUrl = window.location.pathname;
-	
-	fetch(currentUrl, { headers: { 'X-Requested-With': 'XMLHttpRequest' } })
-	  .then(response => response.text())
-	  .then(html => {
-		document.getElementById('game').innerHTML = html;
-	  });
-  });
+  fetch(currentUrl, { headers: { "X-Requested-With": "XMLHttpRequest" } })
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById("game").innerHTML = html;
+    });
+});
 
 document.addEventListener("DOMContentLoaded", (ev) => {
-	loadScript();
+  loadScript();
 });
 
 loadScript();
--- ft_transcendence/ruby_ssr/static/validate_code.js
+++ ft_transcendence/ruby_ssr/static/validate_code.js
@@ -1,64 +1,64 @@
 function loadValidateForm() {
-	if (document.getElementById("form_validate_code")) {
-		document
-			.getElementById("form_validate_code")
-			.addEventListener("submit", function (event) {
-				event.preventDefault();
+  if (document.getElementById("form_validate_code")) {
+    document
+      .getElementById("form_validate_code")
+      .addEventListener("submit", function (event) {
+        event.preventDefault();
 
-				const popUp = document.getElementById("pop-up");
-				popUp.innerHTML = "";
+        const popUp = document.getElementById("pop-up");
+        popUp.innerHTML = "";
 
-				const formData = new FormData(this);
-				const formObject = {};
-				formData.forEach((value, key) => {
-					formObject[key] = value;
-				});
+        const formData = new FormData(this);
+        const formObject = {};
+        formData.forEach((value, key) => {
+          formObject[key] = value;
+        });
 
-				fetch("https://localhost/auth/validate-code", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-						access_token: localStorage.getItem("access_token"),
-					},
-					body: JSON.stringify(formObject),
-				})
-					.then((response) => response.json())
-					.then((data) => {
-						console.log(data);
-						if (data.success) {
-							console.log("Lezzz goo!");
-						} else {
-							popUp.innerHTML = `<div class="alert alert-danger" role="alert">
+        fetch("https://localhost/auth/validate-code", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            access_token: localStorage.getItem("Authorization"),
+          },
+          body: JSON.stringify(formObject),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            if (data.success) {
+              console.log("Lezzz goo!");
+            } else {
+              popUp.innerHTML = `<div class="alert alert-danger" role="alert">
               ${data.error}
               </div>`;
-						}
-					})
-					.catch((error) => console.error("Error:", error));
-			});
-	}
+            }
+          })
+          .catch((error) => console.error("Error:", error));
+      });
+  }
 }
 
 document.addEventListener("DOMContentLoaded", (_) => {
-	if (!document.getElementById("game")) {
-		let copy = document.body.innerHTML;
-		document.body.innerHTML = "";
-		fetch("https://localhost")
-			.then(response => response.text())
-			.then(html => {
-				document.body.innerHTML = html;
-				window.GAMESTATE = 0;
+  if (!document.getElementById("game")) {
+    let copy = document.body.innerHTML;
+    document.body.innerHTML = "";
+    fetch("https://localhost")
+      .then((response) => response.text())
+      .then((html) => {
+        document.body.innerHTML = html;
+        window.GAMESTATE = 0;
 
-				const script = document.body.querySelector("script");
-				const newScript = document.createElement("script");
+        const script = document.body.querySelector("script");
+        const newScript = document.createElement("script");
 
-				newScript.type = "text/javascript";
-				newScript.src = script.src;
-				document.body.appendChild(newScript);
+        newScript.type = "text/javascript";
+        newScript.src = script.src;
+        document.body.appendChild(newScript);
 
-				document.getElementById("game").innerHTML = copy;
-				loadValidateForm();
-			});
-	}
+        document.getElementById("game").innerHTML = copy;
+        loadValidateForm();
+      });
+  }
 });
 
 loadValidateForm();
