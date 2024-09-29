270c9a4312e645c03a3f0926c01436c986f1f1fd
Thomas <thomasribalta@gmail.com>
2024-09-29 15:43:10 +0200

    remove log file, add network in docker, add script to create db in postgress

--- ft_transcendence/postgres/create_tables.sh
+++ ft_transcendence/postgres/create_tables.sh
@@ -1,17 +1,9 @@
-require 'pg'
+#!/bin/bash
 
-sleep(5)
+sleep 5
 
-conn = PG.connect(
-  dbname: ENV['POSTGRES_DB'],
-  user: ENV['POSTGRES_USER'],
-  password: ENV['POSTGRES_PASSWORD'],
-  host: 'postgres',
-  port: 5432
-)
-
-create_user_table_query = <<-SQL
-  CREATE TABLE IF NOT EXISTS _user (
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<-EOSQL
+CREATE TABLE IF NOT EXISTS _user (
     id SERIAL PRIMARY KEY,
     username VARCHAR(12) NOT NULL,
     email VARCHAR(320) NOT NULL UNIQUE,
@@ -22,32 +14,26 @@ create_user_table_query = <<-SQL
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-  );
-SQL
+);
 
-create_email_activation_table_query = <<-SQL
-  CREATE TABLE IF NOT EXISTS _emailActivation (
+CREATE TABLE IF NOT EXISTS _emailActivation (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES _user(id),
     token VARCHAR(6) NOT NULL,
     expire_at TIMESTAMP,
     updated_at TIMESTAMP
-  );
-SQL
+);
 
-create_ranking_table_query = <<-SQL
-  CREATE TABLE IF NOT EXISTS _ranking (
+CREATE TABLE IF NOT EXISTS _ranking (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES _user(id),
     points INTEGER,
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-  );
-SQL
+);
 
-create_game_table_query = <<-SQL
-  CREATE TABLE IF NOT EXISTS _game (
+CREATE TABLE IF NOT EXISTS _game (
     id SERIAL PRIMARY KEY,
     player_1_id INTEGER REFERENCES _user(id),
     player_2_id INTEGER REFERENCES _user(id),
@@ -56,11 +42,9 @@ create_game_table_query = <<-SQL
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-  );
-SQL
+);
 
-create_game_history_table_query = <<-SQL
-  CREATE TABLE IF NOT EXISTS _gameHistory (
+CREATE TABLE IF NOT EXISTS _gameHistory (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES _user(id),
     game_id INTEGER REFERENCES _game(id),
@@ -69,15 +53,7 @@ create_game_history_table_query = <<-SQL
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-  );
-SQL
+);
+EOSQL
 
-conn.exec(create_user_table_query)
-conn.exec(create_email_activation_table_query)
-conn.exec(create_ranking_table_query)
-conn.exec(create_game_table_query)
-conn.exec(create_game_history_table_query)
-
-puts "Tables '_user', '_emailActivation', '_ranking', '_game', et '_gameHistory' vérifiées ou créées avec succès."
-
-conn.close
+echo "Tables '_user', '_emailActivation', '_ranking', '_game', et '_gameHistory' vérifiées ou créées avec succès."
