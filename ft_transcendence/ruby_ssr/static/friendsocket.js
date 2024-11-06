52cc150b9c4fdbec90273f3c142e983c6d1ad76e
Thomas <thomasribalta@gmail.com>
2024-11-06 02:27:59 +0100

    friend online/offline with front fonctionnal

--- ft_transcendence/ruby_ssr/static/friendsocket.js
+++ ft_transcendence/ruby_ssr/static/friendsocket.js
@@ -11,6 +11,28 @@ function connexionFriendSocket() {
     console.log("data : ", event.data);
     let json = JSON.parse(event.data);
     console.log(json);
+    if (json.type === "friend_connected") {
+      let friend = document.getElementById(json.friend);
+      if (friend) {
+        friend.classList.add("online");
+        friend.classList.remove("offline");
+      }
+    }
+    if (json.type === "friend_disconnected") {
+      let friend = document.getElementById(json.friend);
+      if (friend) {
+        friend.classList.add("offline");
+        friend.classList.remove("online");
+      }
+    }
+    if (json.type === "friend_request") {
+      let pop_up = document.getElementById("pop-up");
+      if (pop_up) {
+        pop_up.innerHTML = `<div class="alert alert-info" role="alert">
+            You have received a new friend request.
+          </div>`;
+      }
+    }
   };
 
   connection.onclose = () => {
@@ -20,6 +42,8 @@ function connexionFriendSocket() {
   connection.onerror = (error) => {
     console.error("Erreur WebSocket :", error);
   };
+
+  window.friendSocketConnection = connection;
 }
 
 window.addEventListener("DOMContentLoaded", (_) => {
