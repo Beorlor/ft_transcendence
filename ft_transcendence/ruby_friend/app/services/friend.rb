a3eef3c5ee5f102dea3ca5d4ca7b5abd1abf13fe
Thomas <thomasribalta@gmail.com>
2024-11-08 16:44:32 +0100

    start edit friend systeme

--- ft_transcendence/ruby_friend/app/services/friend.rb
+++ ft_transcendence/ruby_friend/app/services/friend.rb
@@ -2,73 +2,116 @@ require_relative '../log/custom_logger'
 require_relative 'external/user_api'
 
 class Friend
-
   def initialize(logger = Logger.new, user_api = UserApi.new)
     @logger = logger
     @user_api = user_api
-    @user_connections = {}
+    @userlogged = {}
   end
 
-  def add_friend(data)
-    if @user_connections[data['user_id']]
-      @user_connections[data['user_id']].send({type: 'friend_request', username: data['sender_username'], friendship_id: data['friendship_id']}.to_json)
+  def add_friend(data, user_id)
+    if @userlogged[data['friend_id']]
+      @userlogged[data['friend_id']][:ws].send({
+        type: 'friend_request',
+        username: @userlogged[user_id][:username],
+        friendship_id: data['friendship_id']
+      }.to_json)
     end
   end
 
-  def new_friend(data)
-    if @user_connections[data['user_id']]
-      @user_connections[data['user_id']].send({type: 'new_friend', friend_id: data['friend_id'], friendship_id: data['friendship_id']}.to_json)
-      if @user_connections[data['friend_id']]
-        @user_connections[data['friend_id']].send({type: 'friend_connected', friend: data['user_id']}.to_json)
-        @user_connections[data['user_id']].send({type: 'friend_connected', friend: data['friend_id']}.to_json)
+  def send_message(data, user_id)
+    friend_id = data['friend_id']
+    if !@userlogged[user_id][:friends].include?(friend_id)
+      @userlogged[user_id][:ws].send({
+        type: 'error',
+        message: "Cannot send message: the user is not your friend."
+      }.to_json)
+      return
+    end
+    if !@userlogged[friend_id]
+      @userlogged[user_id][:ws].send({
+        type: 'error',
+        message: "Cannot send message: the user is not online."
+      }.to_json)
+      return
+    end
+    @userlogged[friend_id][:ws].send({
+      type: 'message',
+      sender: @userlogged[user_id][:username],
+      message: data['message']
+    }.to_json)
+  end
+
+  def new_friend(data, user_id)
+    if @userlogged[data['user_id']]
+      @userlogged[data['user_id']][:ws].send({
+        type: 'new_friend',
+        friend_id: user_id,
+        friendship_id: data['friendship_id'],
+        status: data['status']
+      }.to_json)
+      if @userlogged[user_id]
+        @userlogged[user_id][:ws].send({
+          type: 'friend_connected',
+          friend: data['user_id']
+        }.to_json)
+        @userlogged[data['user_id']][:ws].send({
+          type: 'friend_connected',
+          friend: user_id
+        }.to_json)
       end
     end
   end
-  
 
   def friend(client, cookie)
     jwt = cookie['access_token']
     @user_api.user_logged(jwt) do |user|
       if user
-        @user_connections[user["user_id"]] = client
+        user_id = user["user_id"]
+        @userlogged[user_id] = { ws: client, friends: [], username: user["username"] }
         @logger.log('Friend', "User connected: #{user}")
-        @user_api.get_user_friends(user["user_id"]) do |friends|
+        @user_api.get_user_friends(user_id) do |friends|
           if friends
-            friends.each do |friend|
-              @logger.log('Friend', "Friend: #{friend}")
-              if @user_connections[friend["requester_id"]] && friend["requester_id"].to_i != user["user_id"].to_i && friend["status"] == "accepted"
-                @user_connections[friend["requester_id"]].send({type: 'friend_connected', friend: user["user_id"]}.to_json)
-                client.send({type: 'friend_connected', friend: friend["requester_id"]}.to_json)
-              elsif @user_connections[friend["receiver_id"]] && friend["receiver_id"].to_i != user["user_id"].to_i && friend["status"] == "accepted"
-                @user_connections[friend["receiver_id"]].send({type: 'friend_connected', friend: user["user_id"]}.to_json)
-                client.send({type: 'friend_connected', friend: friend["receiver_id"]}.to_json)
+            friend_ids = friends
+              .select { |f| f["status"] == "accepted" }
+              .map { |f| f["requester_id"] == user_id ? f["receiver_id"] : f["requester_id"] }
+            @userlogged[user_id][:friends] = friend_ids
+            friend_ids.each do |friend_id|
+              if @userlogged[friend_id]
+                @userlogged[friend_id][:ws].send({
+                  type: 'friend_connected',
+                  friend: user_id
+                }.to_json)
+                client.send({
+                  type: 'friend_connected',
+                  friend: friend_id
+                }.to_json)
               end
             end
           end
         end
         client.onclose do
           @logger.log('Friend', "User disconnected: #{user}")
-          @user_connections.delete(user["user_id"])
-          @user_api.get_user_friends(user["user_id"]) do |friends|
-            if friends
-              friends.each do |friend|
-                if @user_connections[friend["requester_id"]] && friend["requester_id"].to_i != user["user_id"].to_i
-                  @user_connections[friend["requester_id"]].send({type: 'friend_disconnected', friend: user["user_id"]}.to_json)
-                elsif @user_connections[friend["receiver_id"]] && friend["receiver_id"].to_i != user["user_id"].to_i
-                  @user_connections[friend["receiver_id"]].send({type: 'friend_disconnected', friend: user["user_id"]}.to_json)
-                end
-              end
+          @userlogged[user_id][:friends].each do |friend_id|
+            if @userlogged[friend_id]
+              @userlogged[friend_id][:ws].send({
+                type: 'friend_disconnected',
+                friend: user_id
+              }.to_json)
             end
           end
+          @userlogged.delete(user_id)
         end
+
         client.onmessage do |message|
           begin
             data = JSON.parse(message)
             case data['type']
             when "add_friend"
-              add_friend(data)
+              add_friend(data, user_id)
             when "new_friend"
-              new_friend(data)
+              new_friend(data, user_id)
+            when "message"
+              send_message(data, user_id)
             end
           rescue JSON::ParserError => e
             @logger.log('Friend', "Invalid JSON: #{message}")
@@ -77,7 +120,7 @@ class Friend
 
       else
         @logger.log('Friend', "Unauthorized user")
-        client.send({error: 'Unauthorized'}.to_json)
+        client.send({ error: 'Unauthorized' }.to_json)
         client.close
       end
     end
