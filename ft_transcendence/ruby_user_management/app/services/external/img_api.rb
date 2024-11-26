444212ced3b9849d3b35bc0aad5a5082df8dc904
Thomas <thomasribalta@gmail.com>
2024-11-26 10:55:29 +0100

    add delete img

--- ft_transcendence/ruby_user_management/app/services/external/img_api.rb
+++ ft_transcendence/ruby_user_management/app/services/external/img_api.rb
@@ -26,5 +26,31 @@ class ImgApi
       false
     end
   end
+
+  def delete_img(user_id)
+    uri = URI("http://image_ruby:4572/img/delete/#{user_id}")
+    req = Net::HTTP::Post.new(uri)
+    req.content_type = 'application/json'
+  
+    http = Net::HTTP.new(uri.host, uri.port)
+    
+    begin
+      res = http.start { |http| http.request(req) }
+      if res.is_a?(Net::HTTPSuccess)
+        JSON.parse(res.body)
+      else
+        @logger.log('App', "Échec de la suppression de l'image: #{res.code} #{res.message}")
+        false
+      end
+    rescue JSON::ParserError
+      @logger.log('App', "Réponse non-JSON pour la suppression d'image user_id=#{user_id}")
+      false
+    rescue StandardError => e
+      @logger.log('App', "Erreur inattendue lors de la suppression de l'image: #{e.message}")
+      false
+    end
+  end
+  
+  
   
 end
