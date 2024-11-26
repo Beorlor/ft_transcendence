27df0f5e5772579c3024c802b0a844ecc23500c1
zom <contact.zomzd@gmail.com>
2024-11-26 16:58:09 +0100

    Added view informations on rgpd page Added csv download on rgpd page

--- ft_transcendence/ruby_ssr/static/rgpd.js
+++ ft_transcendence/ruby_ssr/static/rgpd.js
@@ -1,11 +1,21 @@
 function handleRGPDRequest(action) {
   let endpoint;
+  const userId = document.getElementById("user_id").value;
   switch (action) {
     case 'access':
       // Supposons que l'ID utilisateur soit disponible dans une variable `userId`
       endpoint = `/api/user/${userId}`;
+      fetch(endpoint, {method: 'GET'})
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            if (res.code == 200)
+              alert(`Username : ${res.user[0].username}\nEmail : ${res.user[0].email}\nProfile Pic : ${res.user[0].img_url}`);
+            else
+              alert("Can't access user informations!");
+        });
       // Redirige vers la page de consultation des données personnelles
-      window.location.href = endpoint;
+      //window.location.href = endpoint;
       break;
     case 'rectify':
       window.location.href = '/edit-profile';
@@ -36,13 +46,31 @@ function handleRGPDRequest(action) {
         });
       break;
     case 'portability':
-      endpoint = `/api/rgpd/portability/${userId}`;
-      window.location.href = endpoint;
+      endpoint = `/api/user/${userId}`;
+      fetch(endpoint, {method: 'GET'})
+        .then(res => res.json())
+        .then(res => {
+            if (res.code == 200) {
+              const rows = [
+                ["Username", "Email", "Profile Picture"],
+                [res.user[0].username, res.user[0].email, res.user[0].img_url]
+              ];
+              let csvContent = rows.map(e => e.join(",")).join("\n");
+              // Create a blob
+              var blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+              var url = URL.createObjectURL(blob);
+
+              // Create a link to download it
+              var pom = document.createElement('a');
+              pom.href = url;
+              pom.setAttribute('download', `${res.user[0].username}_rgpd_informations.csv`);
+              pom.click();
+            }
+            else
+              alert("Can't access user informations!");
+        });
       break;
     default:
       console.error('Action invalide');
   }
 }
-
-// Supposons que `userId` soit défini dans votre template ou défini via JavaScript
-const userId = '<%= @user["id"] %>';
