9e2769489ee599bb6bf171e7afd33660cda4715a
Beorlor <jbdurand2000@gmail.com>
2024-11-26 11:15:19 +0100

    almost perfect elk

--- ft_transcendence/kibana/scripts/import-kibana-config.sh
+++ ft_transcendence/kibana/scripts/import-kibana-config.sh
@@ -7,12 +7,17 @@ until curl -s -o /dev/null "http://localhost:5601/api/status"; do
   sleep 5
 done
 
+sleep 60
 echo "$(date) Kibana is ready. Importing configuration..."
 
-# Import the Kibana configuration
-# curl -X POST "http://localhost:5601/api/saved_objects/_import" \
-#   -H "kbn-xsrf: true" \
-#   -H "Content-Type: application/ndjson" \
-#   --form file=@/usr/share/kibana/config/nginx_access.ndjson
+# Define variables
+KIBANA_URL="http://localhost:5601"
+FILE_PATH="/usr/share/kibana/config/nginx_access.ndjson"
+
+# Perform the import
+curl -X POST "$KIBANA_URL/api/saved_objects/_import" \
+  -H "kbn-xsrf: true" \
+  -H "Content-Type: multipart/form-data" \
+  --form file=@$FILE_PATH
 
 echo "$(date) Kibana configuration imported successfully!"
